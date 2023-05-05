78529c791e46f0371991f2c692da1f83e1f22ab7
LuisMarioSolano <luismariosga@gmail.com>
2023-05-04 22:37:57 -0600

    Convert images to webp

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const autoprefixer = require('autoprefixer');
 
 // Imagenes
 const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp')
 
 function css( done ) {
     // compilar sass
@@ -27,6 +28,12 @@ function imagenes( done ) {
     done();
 }
 
+function versionWebp() {
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( webp() )
+        .pipe( dest('build/img') )
+}
+
 function dev() {
     watch( 'src/scss/**/*.scss', css );
     watch( 'src/img/**/*', imagenes);
@@ -34,7 +41,8 @@ function dev() {
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
-exports.default = series( imagenes, css, dev )
+exports.versionWebp = versionWebp;
+exports.default = series( imagenes, versionWebp, css, dev )
 
 // series - Se inicia una tarea y hasta que finaliza, inicia la siguiente
 // parallel - Todas inician al mismo tiempo
