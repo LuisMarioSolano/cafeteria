c3f522a6283a533648e9fa28946cf96705987a6e
LuisMarioSolano <luismariosga@gmail.com>
2023-05-04 21:00:30 -0600

    Update gulpfile: Add watch all files .scss

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ function css( done ) {
     // compilar sass
     // pasos: 1 - identificar archivo, 2 - Compilarla, 3 - Guardar el .css
     src('src/scss/app.scss')
-        .pipe( sass({ outputStyle: 'compressed' }) )
+        .pipe( sass() )
         .pipe( postcss([ autoprefixer() ]) )
         .pipe( dest('build/css') )
     
@@ -15,7 +15,7 @@ function css( done ) {
 }
 
 function dev() {
-    watch( 'src/scss/app.scss', css );
+    watch( 'src/scss/**/*.scss', css )
 }
 exports.css = css;
 exports.dev = dev;
